edd6ca9b89c0d6f282f8b65930c1961155e302e4
Ilgiz2003 <97854970+Ilgiz2003@users.noreply.github.com>
2023-11-06 17:24:18 +0300

    Initial commit

--- app/src/main/java/com/champions/destiny/musicplayer/MainActivity.kt
+++ app/src/main/java/com/champions/destiny/musicplayer/MainActivity.kt
@@ -65,6 +65,7 @@ class MainActivity : AppCompatActivity() {
             doBindService()
         } else {
             requestPermission()
+            doBindService()
         }
     }
     private fun checkPermission():Boolean{
